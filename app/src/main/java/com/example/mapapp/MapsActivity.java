47dda37d784b3cbc325ba6bc51639ee47f06d9f9
emretnkl <emretnkl94@gmail.com>
2020-03-05 15:47:21 +0300

    Getting long clicked location address.

--- app/src/main/java/com/example/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/mapapp/MapsActivity.java
@@ -26,7 +26,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements GoogleMap.OnMapLongClickListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
     LocationManager locationManager;
@@ -58,17 +58,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,15));
             */
 
-                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
-
-                try {
-                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
-
-                    if (addressList != null && addressList.size() > 0) {
-                        System.out.println("address:" + addressList.get(0).toString());
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
 
 
             }
@@ -102,6 +91,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
 
+        mMap.setOnMapLongClickListener(this);
 
     }
 
@@ -117,5 +107,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+
+        mMap.clear();
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        String address = "";
+
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if (addressList != null && addressList.size() > 0) {
+                if (addressList.get(0).getThoroughfare() != null){
+                    address += addressList.get(0).getThoroughfare();
+                    if (addressList.get(0).getSubThoroughfare() != null){
+                        address += addressList.get(0).getSubThoroughfare();
+                }
+
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (address.matches("")) {
+            address = "No Address";
+        }
+
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
     }
 }
